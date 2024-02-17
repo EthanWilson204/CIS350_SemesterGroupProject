63a0715e4db926bb6c11b0415732d44c317095eb
EthanWilson204 <ehwilson.business@gmail.com>
2024-02-17 16:11:25 -0500

    reversed the pygame version to 2.5.1 to fix the screen taring issue. created primitive screen and backgrounds for each loading bar

--- game.py
+++ game.py
@@ -1,26 +1,50 @@
 import pygame, sys, threading
 from pygame.locals import *
+#initialize pygame
 pygame.init()
 
-
+#display
 screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("Loading Bar")
 
+#font
 FONT = pygame.font.SysFont("Roboto", 100)
+
+#fps
 CLOCK = pygame.time.Clock()
 
 #player = pygame.Rect((300, 250, 50, 50))
-run = True
-while run:
+
+
+#Run the events in game
+while True:
     
+    #check to see if player quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
-          
-    screen.fill("#5592aa")      
+    
+    #screen background   
+    screen.fill("WHITE")      
         
+    #keep the game running. Run at 60 fps
+
+    #    CLOCK.tick(60)     
+
+    WORK = 10000
+
+    #loading bar background
+    #coords for the top left corners of the backgrounds for each loading bar (BGQ# specifies quadrant)
+    LOADING_BGQ1 = pygame.draw.rect(screen, ("RED"), (220,130,200,100))
+    LOADING_BGQ2 = pygame.draw.rect(screen, ("RED"), (860,130,200,100))
+    LOADING_BGQ3 = pygame.draw.rect(screen, ("RED"), (860,490,200,100))
+    LOADING_BGQ4 = pygame.draw.rect(screen, ("RED"), (220,490,200,100))
+    
+    
+    
+
     pygame.display.update()
-    CLOCK.tick(60)     
 
 pygame.quit()
+sys.quit()
