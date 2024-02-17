e2b84af302920112b560f9f962b54adbf73409cf
EthanWilson204 <ehwilson.business@gmail.com>
2024-02-17 15:35:23 -0500

    created a 60fps screen for loading bar

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
-
-import pygame
+import pygame, sys, threading
 from pygame.locals import *
 pygame.init()
 
-screen = pygame.display.set_mode((700,400))
 
-while True:
-    screen.fill(("tan"))
+screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("Loading Bar")
 
+FONT = pygame.font.SysFont("Roboto", 100)
+CLOCK = pygame.time.Clock()
+
+#player = pygame.Rect((300, 250, 50, 50))
+run = True
+while run:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+          
+    screen.fill("#5592aa")      
+        
+    pygame.display.update()
+    CLOCK.tick(60)     
 
-            pygame.quit()
-
-            quit()
-
-        pygame.display.update()
+pygame.quit()
