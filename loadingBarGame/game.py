ead6b6ed6b14c494520b4a953e6efb1934841bd7
Ethan Wilson <ehwilson.business@gmail.com>
2024-02-19 00:08:49 -0500

    deleted backup copy of game.py in separate folder. In game.py in venv, added a side bar and profile corner picture

--- loadingBarGame/game.py
+++ loadingBarGame/game.py
@@ -1,3 +1,8 @@
+'''
+Python Project CIS350
+
+'''
+
 import pygame
 from sys import exit
 #initialize pygame
@@ -7,7 +12,10 @@ pygame.init()
 CLOCK = pygame.time.Clock()
 
 #display
-screen = pygame.display.set_mode((1600,900))
+screen_width = 1600
+screen_height = 900
+
+screen = pygame.display.set_mode((screen_width,screen_height))
 screen.fill('Tan')
 pygame.display.set_caption("Loading Bar")
 
@@ -24,27 +32,46 @@ L1_xpos = start_x
 L1_ypos = 325
 L1_Bar = pygame.Surface((bar_length,bar_height))
 L1_Bar.fill('black')
-L1_speed = 1
+L1_speed = 2
 
 #loading bar 2
 L2_xpos = start_x
 L2_ypos = 425
 L2_Bar = L1_Bar.copy()
 L2_Bar.fill('blue')
-L2_speed = 2
+L2_speed = 3
 
 #loading bar 3
 L3_xpos = start_x
 L3_ypos = 525
 L3_Bar = L1_Bar.copy()
 L3_Bar.fill('green')
-L3_speed = 3
+L3_speed = 4
 
 #button background
 upgrade1_button_background = pygame.Surface((bar_length + 100, bar_height+30))
 upgrade2_button_background = pygame.Surface((bar_length + 100, bar_height+30))
 upgrade3_button_background = pygame.Surface((bar_length + 100, bar_height+30))
 
+#upper bar display
+Profile_Bar = pygame.Surface((screen_width, 200))
+Profile_Bar.fill((160, 160, 160))
+
+#left side status bar display
+Left_Side_Bar = pygame.Surface((250, screen_height - 200))
+Left_Side_Bar.fill((101, 132, 129))
+
+#profile corner display
+Profile_Corner = pygame.Surface((250,200))
+Profile_Corner.fill((176, 98, 108))
+
+#static screen behavior    
+screen.blit(upgrade1_button_background,(load_limit + 100, L1_ypos - 15))
+screen.blit(upgrade2_button_background,(load_limit + 100, L2_ypos - 15))
+screen.blit(upgrade3_button_background,(load_limit + 100, L3_ypos - 15))
+screen.blit(Profile_Bar, (0,0))
+screen.blit(Profile_Corner, (0,0))
+screen.blit(Left_Side_Bar, (0,200))
 
 #Run the events in game
 while True:
@@ -53,10 +80,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
-    
-    screen.blit(upgrade1_button_background,(load_limit + 100, L1_ypos - 15))
-    screen.blit(upgrade2_button_background,(load_limit + 100, L2_ypos - 15))
-    screen.blit(upgrade3_button_background,(load_limit + 100, L3_ypos - 15))
     
     #loading bar 1
     L1_xpos += L1_speed
