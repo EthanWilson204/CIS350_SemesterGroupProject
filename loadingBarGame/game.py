0d52c89ccf2a2695b786ee6a5fb9667c93946e3b
EthanWilson204 <ehwilson.business@gmail.com>
2024-02-19 12:26:18 -0500

    added rectanges for button surfaces

--- loadingBarGame/game.py
+++ loadingBarGame/game.py
@@ -34,12 +34,14 @@ L1_Bar = pygame.Surface((bar_length,bar_height))
 L1_Bar.fill('black')
 L1_speed = 2
 
+
 #loading bar 2
 L2_xpos = start_x
 L2_ypos = 425
 L2_Bar = L1_Bar.copy()
 L2_Bar.fill('blue')
 L2_speed = 3
+
 
 #loading bar 3
 L3_xpos = start_x
@@ -48,27 +50,44 @@ L3_Bar = L1_Bar.copy()
 L3_Bar.fill('green')
 L3_speed = 4
 
+
+#bar background
+pygame.draw.rect(screen, 'red', (L1_xpos-15, L1_ypos-15, 780, bar_height+30))
+pygame.draw.rect(screen, 'red', (L2_xpos-15, L2_ypos-15, 780, bar_height+30))
+pygame.draw.rect(screen, 'red', (L3_xpos-15, L3_ypos-15, 780, bar_height+30))
+pygame.draw.rect(screen, 'tan', (L1_xpos, L1_ypos, 740, bar_height))
+pygame.draw.rect(screen, 'tan', (L2_xpos, L2_ypos, 740, bar_height))
+pygame.draw.rect(screen, 'tan', (L3_xpos, L3_ypos, 740, bar_height))
+
+
 #button background
 upgrade1_button_background = pygame.Surface((bar_length + 100, bar_height+30))
 upgrade2_button_background = pygame.Surface((bar_length + 100, bar_height+30))
 upgrade3_button_background = pygame.Surface((bar_length + 100, bar_height+30))
+B1_rect = upgrade1_button_background.get_rect(topleft = (load_limit + 100, L1_ypos - 15))
+B2_rect = upgrade2_button_background.get_rect(topleft = (load_limit + 100, L2_ypos - 15))
+B3_rect = upgrade3_button_background.get_rect(topleft = (load_limit + 100, L3_ypos - 15))
+
 
 #upper bar display
 Profile_Bar = pygame.Surface((screen_width, 200))
 Profile_Bar.fill((160, 160, 160))
 
+
 #left side status bar display
 Left_Side_Bar = pygame.Surface((250, screen_height - 200))
 Left_Side_Bar.fill((101, 132, 129))
+
 
 #profile corner display
 Profile_Corner = pygame.Surface((250,200))
 Profile_Corner.fill((176, 98, 108))
 
+
 #static screen behavior    
-screen.blit(upgrade1_button_background,(load_limit + 100, L1_ypos - 15))
-screen.blit(upgrade2_button_background,(load_limit + 100, L2_ypos - 15))
-screen.blit(upgrade3_button_background,(load_limit + 100, L3_ypos - 15))
+screen.blit(upgrade1_button_background, B1_rect)
+screen.blit(upgrade2_button_background, B2_rect)
+screen.blit(upgrade3_button_background, B3_rect)
 screen.blit(Profile_Bar, (0,0))
 screen.blit(Profile_Corner, (0,0))
 screen.blit(Left_Side_Bar, (0,200))
@@ -110,20 +129,3 @@ while True:
 
 pygame.quit()
 
-
-
-
-'''
-#loading bar background
-#coords for the top left corners of the backgrounds for each loading bar (BGQ# specifies quadrant)
-LOADING_BGQ1 = pygame.draw.rect(screen, ("RED"), (x_coord_Q1Q4, y_coord_Q1Q2-200, bar_width, bar_height))
-LOADING_BGQ2 = pygame.draw.rect(screen, ("RED"), (x_coord_Q2Q3, y_coord_Q1Q2-200, bar_width, bar_height))
-LOADING_BGQ3 = pygame.draw.rect(screen, ("RED"), (x_coord_Q1Q4, y_coord_Q3Q4, bar_width,bar_height))
-LOADING_BGQ4 = pygame.draw.rect(screen, ("RED"), (x_coord_Q2Q3, y_coord_Q3Q4, bar_width,bar_height))
-
-#upgrade button background
-LOADING_BGQ1 = pygame.draw.rect(screen, ("BLUE"), (x_coord_Q1Q4 + bar_width + 30, y_coord_Q1Q2-200, 100, bar_height))
-LOADING_BGQ2 = pygame.draw.rect(screen, ("BLUE"), (x_coord_Q2Q3 + bar_width + 30, y_coord_Q1Q2-200, 100, bar_height))
-LOADING_BGQ3 = pygame.draw.rect(screen, ("BLUE"), (x_coord_Q1Q4 + bar_width + 30, y_coord_Q3Q4, 100, bar_height))
-LOADING_BGQ4 = pygame.draw.rect(screen, ("BLUE"), (x_coord_Q2Q3 + bar_width + 30, y_coord_Q3Q4, 100, bar_height))
-'''
